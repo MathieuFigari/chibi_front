c0e6988b3766bfbe264e34e17e5ac8b2b9fd72c2
Mathieu Figari <malenazeng@gmail.com>
2022-04-24 08:22:32 +0200

    stripekeyUpdate

--- src/components/StripeContainer/index.js
+++ src/components/StripeContainer/index.js
@@ -2,7 +2,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "../CheckoutForm";
 
-const PUBLIC_KEY = "pk_test_51JeKakDij0gh8k8y8yb86FD0xb6QYq7Uq6WjgVb1Hf4rqFtUfiCfMdQUMEHSsEWk1puNNQf2q7uUwDhoUf7r2W7z00ajjIPQip";
+const PUBLIC_KEY = "pk_test_51KiIbcA9uMX4IedOHAf1y4TS2szKjDwuouHW5WyFrW20MNqyQ6CZYuKhSd76D1HMvAYGPE7iILNmes52QMmMea7300VUPocbq9";
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
